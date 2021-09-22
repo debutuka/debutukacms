d2c71c916c48f9885ff4148bf681e9a2ea46c8ae
Isak Wang Gustavsen <69854945+isakgustavsen@users.noreply.github.com>
2021-09-23 00:48:07 +0200

    Initial Commit

---
+++ schemas/konsert.js
@@ -0,0 +1,65 @@
+export default {
+  name: 'konsert',
+  title: 'Konsert',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'artist',
+      title: 'Artist',
+      type: 'reference',
+      to: {type: 'artist'},
+    },
+    {
+      name: 'mainImage',
+      title: 'Main image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'categories',
+      title: 'Categories',
+      type: 'array',
+      of: [{type: 'reference', to: {type: 'category'}}],
+    },
+    {
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime',
+    },
+    {
+      name: 'body',
+      title: 'Body',
+      type: 'blockContent',
+    },
+  ],
+
+  preview: {
+    select: {
+      title: 'title',
+      author: 'artist.name',
+      media: 'mainImage',
+    },
+    prepare(selection) {
+      const {artist} = selection
+      return Object.assign({}, selection, {
+        subtitle: artist && `by ${artist}`,
+      })
+    },
+  },
+}
